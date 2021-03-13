c0f03f5ae9ba1cd8e6a8e43bcf115a50fab1f683
TakoYuxin <31773666+TakoYuxin@users.noreply.github.com>
2021-03-12 16:22:01 -0800

    Update server.js
    change the order of port OR statement

--- server.js
+++ server.js
@@ -81,6 +81,6 @@ io.on('connection', socket => {
 
 });
 
-const PORT = 4000 || process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
