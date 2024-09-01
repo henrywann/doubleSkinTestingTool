3c4407bb768da34fea15c59a2e6601b84dda9a61
henrywann <henrybryantwang@gmail.com>
2024-09-01 09:18:23 -0700

    added display in drop down

--- public/js/home.js
+++ public/js/home.js
@@ -1,41 +1,57 @@
 const socket = io();
-const goodCardsSelected = document.getElementById('goodCards');
-const numberOfPlayersSelected = document.getElementById('numOfPlayers');
+const goodCardsSelected = document.getElementById("goodCards");
+const numberOfPlayersSelectedDropDown = document.getElementById("numOfPlayers");
+const badIdentitiesSelectedDropDown = document.getElementById("badIdentities");
 
 socket.on("connect", () => {
-    console.log(socket.id);
-    socket.emit('initialGoodCards');
+  console.log(socket.id);
+  socket.emit("initialGoodCards");
 });
 
-socket.on('displaySelectedCardsEvent', goodPlayerCardList => {
-    displaySelectedCards(goodPlayerCardList);
+socket.on("displaySelectedCardsEvent", (goodPlayerCardList) => {
+  displaySelectedCards(goodPlayerCardList);
 });
 
-socket.on('displaySelectedNumberOfPlayers', numberOfPlayers => {
-    displayNumberOfPlayers(numberOfPlayers);
+socket.on("displaySelectedNumberOfPlayers", (numberOfPlayers) => {
+  displayNumberOfPlayersFromDropDown(numberOfPlayers);
+});
+
+socket.on("displaySelectedBadIdentities", (badIdentities) => {
+  displayBadIdentitiesFromDropDown(badIdentities);
 });
 
 function selectCard(card) {
-    socket.emit('selectGoodCard', card);
+  socket.emit("selectGoodCard", card);
 }
 
 function displaySelectedCards(selectedCardList) {
-    goodCardsSelected.innerHTML = '';
-    selectedCardList.forEach(card => {
-        const li = document.createElement('li');
-        li.innerText = card;
-        goodCardsSelected.appendChild(li);
-    });
+  goodCardsSelected.innerHTML = "";
+  selectedCardList.forEach((card) => {
+    const li = document.createElement("li");
+    li.innerText = card;
+    goodCardsSelected.appendChild(li);
+  });
 }
 
-function selectNumberOfPlayers(numberOfPlayers) {
-    socket.emit('selectNumberOfPlayers', numberOfPlayers);
+function selectNumberOfPlayersFromDropDown() {
+  var text = numberOfPlayersSelectedDropDown.options[numberOfPlayersSelectedDropDown.selectedIndex].text;
+  socket.emit("selectNumberOfPlayers", text);
 }
 
-function displayNumberOfPlayers(numberOfPlayers) {
-    numberOfPlayersSelected.innerHTML = '';
-    var option = document.createElement('option');
-    option.innerText = numberOfPlayers;
-    option.setAttribute('value', numberOfPlayers);
-    numberOfPlayersSelected.appendChild(option);
+function displayNumberOfPlayersFromDropDown(numberOfPlayers) {
+  numberOfPlayersSelectedDropDown.value = numberOfPlayers;
+  if (numberOfPlayers === '7') {
+    badIdentitiesSelectedDropDown.disabled = false;
+  } else {
+    badIdentitiesSelectedDropDown.disabled = true;
+  }
+}
+
+function selectBadIdentitiesFromDropDown() {
+  var value = badIdentitiesSelectedDropDown.value;
+  socket.emit("selectedBadIdentities", value);
+}
+
+function displayBadIdentitiesFromDropDown(badIdentities) {
+  badIdentitiesSelectedDropDown.value = badIdentities;
 }
